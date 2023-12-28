966c774b8ccab17837a3e422942dd7d1f64e170b
GongJinZheng <9075512@qq.com>
2023-12-28 14:54:34 +0800

    v1.0.0 初始化项目

---
+++ src/views/indexs/index.vue
@@ -0,0 +1,81 @@
+
+<template>
+  <div class="contents">
+    <div class="contetn_right">
+      <ItemWrap
+        class="contetn_left-bottom contetn_lr-item"
+        title="报警次数"
+      >
+        <RightTop />
+      </ItemWrap>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import LeftTop from './left-top.vue'
+import LeftCenter from "./left-center.vue";
+import LeftBottom from "./left-bottom.vue";
+import CenterMap from "./center-map.vue";
+import CenterBottom from "./center-bottom.vue";
+import RightTop from "./right-top.vue";
+import RightCenter from "./right-center.vue";
+import RightBottom from "./right-bottom.vue";
+
+export default {
+  components: {
+    LeftTop,
+    LeftCenter,
+    LeftBottom,
+    CenterMap,
+    RightTop,
+    RightCenter,
+    RightBottom,
+    CenterBottom,
+  },
+  data() {
+    return {
+    
+    };
+  },
+  filters: {
+    numsFilter(msg) {
+      return msg || 0;
+    },
+  },
+  created() {
+  },
+
+  mounted() {},
+  methods: {
+  
+  },
+};
+</script>
+<style lang="scss" scoped>
+// 内容
+.contents {
+  .contetn_right {
+    width: 1920px;
+    box-sizing: border-box;
+    // padding: 16px 0;
+  }
+}
+
+
+@keyframes rotating {
+    0% {
+        -webkit-transform: rotate(0) scale(1);
+        transform: rotate(0) scale(1);
+    }
+    50% {
+        -webkit-transform: rotate(180deg) scale(1.1);
+        transform: rotate(180deg) scale(1.1);
+    }
+    100% {
+        -webkit-transform: rotate(360deg) scale(1);
+        transform: rotate(360deg) scale(1);
+    }
+}
+</style>
